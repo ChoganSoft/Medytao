21391f77346cc798af4328ec76870785e4325d12
MariuszCh <ja@mariusz.ch>
2026-04-22 12:19:50 +0200

    chore(db): switch dev database from LocalDB to SQL Server Express
    - Update connection string in appsettings.json to use .\SQLEXPRESS
    - Rename dev database from Medytao to Medytao.Db
    - Bump version to 0.10.2

--- src/Medytao.Api/obj/Debug/net9.0/Medytao.Api.AssemblyInfo.cs
+++ src/Medytao.Api/obj/Debug/net9.0/Medytao.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medytao.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e156f69374625b39f22f085988886e4946b4b545")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medytao.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medytao.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Medytao.Infrastructure/obj/Debug/net9.0/Medytao.Infrastructure.AssemblyInfo.cs
+++ src/Medytao.Infrastructure/obj/Debug/net9.0/Medytao.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medytao.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e156f69374625b39f22f085988886e4946b4b545")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medytao.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medytao.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
